66e46300f782a2834239d541d152f6a9301dc18a
Mandela Pulei <mandelapulei@gmail.com>
2026-02-23 18:16:18 +0300

    feat:Import pandas and load client/price dataset

---
+++ main_analysis.py
@@ -0,0 +1,8 @@
+## Customer-Churn Analysis Project
+## Author: Mandela Pulei
+
+# Loading Necessary libraries
+
+   import pandas as pd
+   client_df = pd.read_csv("client_data.csv")
+   price_df  = pd.read_csv("price_data.csv")
